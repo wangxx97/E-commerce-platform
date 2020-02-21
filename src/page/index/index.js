13fad5596e8023adb917a799c517a1c9970a7145
wangxixin <192390475@qq.coom>
2020-02-21 11:18:33 +0800

    webpack配置大概完成

---
+++ src/page/index/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!*/
+// var $ = require('jquery');
+// $('body').html('HELLO INDEX');
+
+// var $$ = require('jquery');
+// console.log('Hello index');
+// $$('body').html('index HELLO~~~~~~~~~~~~~~~');
+
+require('./index.css');
+require('../module.js');
