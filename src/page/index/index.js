f82bb7c3afce8e610fa6a9b916f4833d4530cfcd
wangxixin <192390475@qq.coom>
2020-02-22 13:25:08 +0800

    基本配置

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -14,13 +14,29 @@
 var _mm = require('util/mm');
 // alert(123);
 
-_mm.request({
-    url: './test.do',
-    success: function (res) {
-        console.log(res);
-    },
-    error: function (errMsg) {
-        console.log(errMsg)
-    }
-});
+// _mm.request({
+//     url: './test.do',
+//     success: function (res) {
+//         console.log(res);
+//     },
+//     error: function (errMsg) {
+//         console.log(errMsg)
+//     }
+// });
+//
+
+// console.log(_mm.getUrlParam('test'));
+
+var html = '<div>{{data}}</div>';
+var data = {
+    data: 'test'
+}
+
+console.log(_mm.renderHtml(html, data));
+
+
+
+
+
+
 
