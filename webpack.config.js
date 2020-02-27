dc17ff34b5058d4b341f2da75ea4966e519fd314
wangxixin <192390475@qq.coom>
2020-02-27 18:39:41 +0800

    操作页面

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,11 @@ var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
 console.log(WEBPACK_ENV);
 
 // 获取html-webpack-plugin参数方法
-var getHtmlConfig = function (name) {
+var getHtmlConfig = function (name,title) {
     return {
         template: './src/view/' + name + '.html',
         filename: 'view/' + name + '.html',
+        title       : title,
         inject: true,
         hash: true,
         chunks: ['common', name]
@@ -21,7 +22,9 @@ var config = {
     entry: {
         'common': ['./src/page/common/index.js'],
         'index': ['./src/page/index/index.js'],
-        'login': ['./src/page/login/index.js']
+        'login': ['./src/page/login/index.js'],
+        'result': ['./src/page/result/index.js'],
+
     },
     output: {
         path: './dist',
@@ -56,9 +59,9 @@ var config = {
         // 把css单独打包到文件里
         new ExtractTextPlugin("css/[name].css"),
         // html模板的处理
-        new HtmlWebpackPlugin(getHtmlConfig('index')),
-        new HtmlWebpackPlugin(getHtmlConfig('login')),
-
+        new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
+        new HtmlWebpackPlugin(getHtmlConfig('login','用户登陆')),
+        new HtmlWebpackPlugin(getHtmlConfig('result','操作结果')),
     ]
 };
 
